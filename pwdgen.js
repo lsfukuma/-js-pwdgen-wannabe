75edbe31f11ff8fca052df344cc0358977f0a304
Leticia <lsf@protonmail.ch>
2020-04-17 15:20:54 +0200

    esercizio js

---
+++ pwdgen.js
@@ -0,0 +1,12 @@
+ //chiedere all'utente il suo nome
+var nome = prompt ('Qual è il tuo nome?')
+
+ //chiedere all'utente il suo cognome
+var cognome = prompt ('Qual è il tuo cognome?')
+
+ //chiedere all'utente il suo colore preferito
+var colore = prompt ('Qual è il tuo colore preferito?')
+
+ //visualizzare le risposte + 20
+console.log (nome + cognome + colore + '20')
+document.writeln(nome, cognome, colore, '20')
